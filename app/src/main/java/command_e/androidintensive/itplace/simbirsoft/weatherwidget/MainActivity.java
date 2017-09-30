5b7f52937204a0b069decb8fe774a0e91bd951fd
artjkee73 <artjkee73@gmail.com>
2017-09-30 17:25:00 +0400

    clean project

---
+++ app/src/main/java/command_e/androidintensive/itplace/simbirsoft/weatherwidget/MainActivity.java
@@ -0,0 +1,13 @@
+package command_e.androidintensive.itplace.simbirsoft.weatherwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
